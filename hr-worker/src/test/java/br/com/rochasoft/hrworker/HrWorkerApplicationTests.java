0341540bcfc6d60b97df750ea5539eea3bbbd3e0
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-04-09 20:00:33 -0300

    projeto hr-worker criado

---
+++ hr-worker/src/test/java/br/com/rochasoft/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rochasoft.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
