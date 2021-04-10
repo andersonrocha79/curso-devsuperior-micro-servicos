b32ebc537c149f9b8295f38aa9e2882521d84cda
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-04-10 08:33:37 -0300

    inclusão do balanceamento de carga com o Ribbon

--- hr-payroll/src/main/java/br/com/rochasoft/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/br/com/rochasoft/hrpayroll/feignclients/WorkerFeignClient.java
@@ -11,7 +11,7 @@ import br.com.rochasoft.hrpayroll.entities.Worker;
 // interface com os métodos 'disponíveis' no projeto 'hr-worker'
 
 @Component // componente gerenciado pelo spring (será injetado em outras classes)
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient 
 {
 	
@@ -19,3 +19,18 @@ public interface WorkerFeignClient
 	ResponseEntity<Worker> findById(@PathVariable Long id);	
 
 }
+
+
+/*
+  
+classe inicial quando utilizava RestTemplate
+@Component // componente gerenciado pelo spring (será injetado em outras classes)
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient 
+{
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);	
+}
+
+  
+*/
--- hr-worker/src/main/java/br/com/rochasoft/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/rochasoft/hrworker/resources/WorkerResource.java
@@ -2,7 +2,9 @@ package br.com.rochasoft.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,12 @@ import br.com.rochasoft.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource 
 {
+	
+	// habilita o log nesta classe
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env; // informações de contexto da aplicação
 	
 	@Autowired
 	private WorkerRepository repository;
@@ -35,6 +43,9 @@ public class WorkerResource
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id)
 	{
+		
+		// imprime a porta que está rodando
+		logger.info("porta = " + env.getProperty("local.server.port"));		
 		
 		Worker obj = repository.findById(id).get();
 		
