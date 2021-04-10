7131a5e26c5157ca05ba863e1c7ddf22613ac55d
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-04-10 08:48:20 -0300

    configurando Eureka Server

---
+++ hr-eureka-server/src/test/java/br/com/rochasoft/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rochasoft.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
