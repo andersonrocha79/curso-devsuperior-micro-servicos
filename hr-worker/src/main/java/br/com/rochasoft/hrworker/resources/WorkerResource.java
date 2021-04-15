ef6abe5246d06867fac74f076edd6c8629aece99
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-04-14 21:19:21 -0300

    inclusão do microserviço de configuração

--- hr-worker/src/main/java/br/com/rochasoft/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/rochasoft/hrworker/resources/WorkerResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.rochasoft.hrworker.entities.Worker;
 import br.com.rochasoft.hrworker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource 
@@ -21,6 +24,9 @@ public class WorkerResource
 	
 	// habilita o log nesta classe
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private Environment env; // informações de contexto da aplicação
@@ -76,6 +82,16 @@ public class WorkerResource
 		logger.info("final método findById");
 		
 		return ResponseEntity.ok(obj);				
+		
+	}	
+	
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfigs()
+	{
+		
+		logger.info("CONFIGURAÇÃO DO HR-CONFIG-SERVER: " + testConfig);
+		
+		return ResponseEntity.noContent().build();
 		
 	}	
 	
