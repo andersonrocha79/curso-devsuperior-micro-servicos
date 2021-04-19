29bb8eaf52083a0deb21fcc32984dc3206407895
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-04-19 19:57:28 -0300

    inclusão do token JWT

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.devsuperior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignClients.UserFeignClient;
 
 @Service // componente do spring
-public class UserService 
+public class UserService implements UserDetailsService
 {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
@@ -32,6 +35,25 @@ public class UserService
 		
 		logger.error("e-mail encontrado {}", email);
 		return user;
+		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException 
+	{
+		
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if (user == null)
+		{
+			logger.error("e-mail não encontrado {}", username);
+			throw new UsernameNotFoundException("e-mail não encontrado");
+		}
+		
+		logger.error("e-mail encontrado {}", username);
+		
+		return user;		
+		
 		
 	}
 
